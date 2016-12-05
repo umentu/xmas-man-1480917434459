89a9577547512e6ee365abbc0f66f85f5d5b553e
umentu <umentu@umentu-no-MacBook-Pro.local>
2016-12-05 21:00:08 +0900

    second commit

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import os
 try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
+    from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
+    from SocketServer import TCPServer as Server
 except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
 
 # Read port selected by the cloud for our application
 PORT = int(os.getenv('PORT', 8000))
